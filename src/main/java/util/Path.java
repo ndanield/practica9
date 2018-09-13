1a579bbdc19fa6ad7b4cc50a9b8c23664d39ba4b
ndanield <ndanield@hotmail.com>
2018-09-12 21:43:19 -0400

    Vistas preparadas

--- src/main/java/util/Path.java
+++ src/main/java/util/Path.java
@@ -3,5 +3,5 @@ package util;
 public class Path {
     public final static String POLL = "poll.ftl";
     public final static String POLL_LISTING = "pollListing.ftl";
-    public final static String POLL_LOCATION = "location.ftl";
+    public final static String POLL_LOCATION = "pollLocation.ftl";
 }
